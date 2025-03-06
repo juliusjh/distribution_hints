85c5f278b7f3e3ff881b8787f8fb55fb59e3fb48
Julius Hermelink <julius.hermelink@pm.me>
2025-03-06 16:19:28 +0100

    Fix build_rust.sh

--- build_rust.sh
+++ build_rust.sh
@@ -1,4 +1,3 @@
-cd hint_solver && RUSTFLAGS=" -C target-cpu=native" maturin develop --release
-cd ..
-cd rust_python_kyber && RUSTFLAGS=" -C target-cpu=native" maturin develop --release
-cd ..
+(cd rust_python_kyber/PQClean/crypto_kem/kyber768/clean && make)
+(cd hint_solver && RUSTFLAGS=" -C target-cpu=native" maturin develop --release)
+(cd rust_python_kyber && RUSTFLAGS=" -C target-cpu=native" maturin develop --release)
